8588e2a7b1b2b27d8f6463fe9908980ea1dc92a8
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2016-03-13 11:05:51 +0100

    wip on python module, not much time, contraction time

--- src/pyDealer/pyDealer.go
+++ src/pyDealer/pyDealer.go
@@ -1,2 +1,49 @@
 // See https://blog.filippo.io/building-python-modules-with-go-1-5/#thecompletedemosource
-// TBD
+// and https://gopy.qur.me/extensions/examples.html
+
+
+package main
+
+import (
+	"bufio"
+	"dealer"
+	"encoding/json"
+	"fmt"
+  "gopy"
+	"net"
+	"strconv"
+	"time"
+)
+
+struct PySocket {
+  socket  dealer.Socket
+  port    string
+  address string
+}
+
+func Connect(s *PySocket, args *py.Tuple)(err Error) {
+  // Parse python arguments
+  var o py.Object
+  if err := py.ParseTuple(args, "O", &o); err != nil {
+    fmt.Println("Error parsing arguments")
+  }
+
+  // Connect dealer
+  s.socket = dealer.Socket{}
+  if err = s.socket.Connect( o[0], o[1]); err != nil {
+    fmt.Println("Error creating the socket")
+  }
+}
+
+func Read(s * PySocket, list *py.List) (error Error){
+  // TODO: Ben
+  // We need to grab the ID here, and do a ReadJSON on the dealer socket
+}
+
+func Send(s * PySocket, list *py.List) (error Error){
+  // TODO: Ben
+}
+
+func Close(s *PySocket) {
+  s.socket.Close()
+}
